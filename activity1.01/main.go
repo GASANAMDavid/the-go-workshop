0ab908dbc0d667446f4d9f3191a9cfb4842345f7
David Gasana Manzi <gmdavid59@gmail.com>
2022-08-04 19:48:22 +0200

    Activity 1.01

---
+++ activity1.01/main.go
@@ -0,0 +1,15 @@
+package main
+
+import "fmt"
+
+func main() {
+	var first_name string = "Bob"
+	var family_name string = "Smith"
+	age := 34
+	peanut_allergy := false
+
+	fmt.Println(first_name)
+	fmt.Println(family_name)
+	fmt.Println(age)
+	fmt.Println(peanut_allergy)
+}
